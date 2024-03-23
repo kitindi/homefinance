0442eadfa92d7b0c24499e8944dbd67737997323
kitindi <abdullkitindi@gmail.com>
2024-03-24 01:42:18 +0300

    added dashboard ui

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.dashboard_view, name='dashboard'),
+    path("expenses/", views.all_expenses, name='all-expenses'),
+    path("expenses/add/", views.add_expenses, name='add-expenses'),
+    path("expenses/edit/<int:pk>", views.edit_expenses, name='edit-expenses'),
+    path("expenses/delete/<int:pk>", views.delete_expenses, name='delete-expenses'),
 ]
--- main/views.py
+++ main/views.py
@@ -6,3 +6,17 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='login')
 def dashboard_view(request):
     return render(request, 'main/dashboard.html')
+
+
+def all_expenses(request):
+    pass
+
+def add_expenses(request):
+    pass
+
+def edit_expenses(request,pk):
+    pass
+
+
+def delete_expenses(request,pk):
+    pass
